30e6ad42310e3535a0c70f6290f22d093450f796
MeteHanBicer <67206744+MeteHanBicer@users.noreply.github.com>
2021-03-22 10:34:06 +0300

    commit v2

--- barchart/src/app/app.module.ts
+++ barchart/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarchartComponent } from './barchart/barchart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarchartComponent
   ],
   imports: [
     BrowserModule,
