e33f6c099dfa8120559de9789054c3edf70351d0
MeteHanBicer <67206744+MeteHanBicer@users.noreply.github.com>
2021-03-22 17:04:29 +0300

    commit v4 bar-chart

--- barchart/src/app/barchart/barchart.component.ts
+++ barchart/src/app/barchart/barchart.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+const SAMPLE_BARCHART_DATA:any[] = [
+  // { data: [2016,2017,2019,2020,2021],label:'Q3 Sales'},
+  { data: [25,39,60,91,36,54,50],label:'Aktif Değer'},
+  { data: [25,50,60,91,36,54,50],label:'Pasif Değer'}
+];
+
+const SAMPLE_BARCHART_LABELS : string[] = [ '2015','2021'];
 
 @Component({
   selector: 'app-barchart',
@@ -8,6 +15,15 @@ import { Component, OnInit } from '@angular/core';
 export class BarchartComponent implements OnInit {
 
   constructor() { }
+
+    public barChartData : any[] = SAMPLE_BARCHART_DATA;
+    public barChartLabels:string[] = SAMPLE_BARCHART_LABELS;
+    public barChartType = 'bar';
+    public barChartLegend = false;
+    public barChartOptions:any = {
+      scaleShowVerticalLines : false,
+      resonsive : true
+    };
 
   ngOnInit() {
   }
