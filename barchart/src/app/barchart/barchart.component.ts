9bd1a9f3e593e83943a6e680a758f5da62affd57
MeteHanBicer <67206744+MeteHanBicer@users.noreply.github.com>
2021-03-23 09:55:40 +0300

    commit v7 deneme pie chart

--- barchart/src/app/barchart/barchart.component.ts
+++ barchart/src/app/barchart/barchart.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-const SAMPLE_BARCHART_DATA:any[] = [
-  // { data: [2016,2017,2019,2020,2021],label:'Q3 Sales'},
-  { data: [25,39,60,91,36,54,50],label:'Aktif Değer'},
-  { data: [25,50,60,91,36,54,50],label:'Pasif Değer'}
-];
+// BAR CHAR
+// const SAMPLE_BARCHART_DATA:any[] = [
+//   // { data: [2016,2017,2019,2020,2021],label:'Q3 Sales'},
+//   { data: [25,39,60,91,36,54,50],label:'Aktif Değer'},
+//   { data: [25,50,60,91,36,54,50],label:'Pasif Değer'}
+// ];
 
 
 @Component({
@@ -12,22 +13,42 @@ const SAMPLE_BARCHART_DATA:any[] = [
   styleUrls: ['./barchart.component.css']
 })
 export class BarchartComponent implements OnInit {
+  public chartType: string = 'pie';
 
+  public chartDatasets: Array<any> = [
+    { data: [300, 50, 100, 40, 120], label: 'My First dataset' }
+  ];
+
+  public chartLabels: Array<any> = ['Red', 'Green', 'Yellow', 'Grey', 'Dark Grey'];
+
+  public chartColors: Array<any> = [
+    {
+      backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'],
+      hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#A8B3C5', '#616774'],
+      borderWidth: 2,
+    }
+  ];
+
+  public chartOptions: any = {
+    responsive: true
+  };
+  public chartClicked(e: any): void { }
+  public chartHovered(e: any): void { }
   constructor() { }
 
-    public barChartData : any[] = SAMPLE_BARCHART_DATA;
-    public barChartType = 'bar';
-    public barChartLegend = false;
-    public barChartOptions:any = {
-      scaleShowVerticalLines : false,
-      resonsive : true
-    };
+    // public barChartData : any[] = SAMPLE_BARCHART_DATA;
+    // public barChartType = 'bar';
+    // public barChartLegend = false;
+    // public barChartOptions:any = {
+    //   scaleShowVerticalLines : false,
+    //   resonsive : true
+    // };barchar
 
   ngOnInit() {
   }
   alert(yil:number,aktif:number,pasif:number){
     alert("Yıl : "+yil +" Aktif : "+aktif+" Pasif : "+pasif)
   }
-  public SAMPLE_BARCHART_LABELS : string[] = [ '2015','2021'];
-  public barChartLabels:string[] = this.SAMPLE_BARCHART_LABELS;
+  // public SAMPLE_BARCHART_LABELS : string[] = [ '2015','2021'];
+  // public barChartLabels:string[] = this.SAMPLE_BARCHART_LABELS;barchar
 }
